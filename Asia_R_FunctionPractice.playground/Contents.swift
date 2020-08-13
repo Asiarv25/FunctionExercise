dd2ace5ee047c45f8dbc88416da5865523596204
Asiarv25 <67976040+Asiarv25@users.noreply.github.com>
2020-08-13 14:18:56 -0500

    Add files via upload

---
+++ Asia_R_FunctionPractice.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+func JacketPrice(Price: [Int] ) -> Int
+{
+    let Price = 25
+   
+    func Clearance (Discount: [Int])-> Int{
+        let Discount = 0.50 * JacketPrice(Price:25)
+        
+
+        
+        
+        
+    
+}
+
